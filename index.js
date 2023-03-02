194dbf965fcb10b42b8062cae4b2565aae5f298a
Seth Reid <reid.seth.96@gmail.com>
2023-03-01 17:07:34 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,51 @@
+//element pulls
+let form = document.querySelector('form')
+let inputField = document.querySelector('input')
+let ul = document.querySelector('ul')
+let message = document.querySelector('#message')
+
+//funtions
+function addMovie(event){
+    event.preventDefault()
+    let movie = document.createElement('li')
+    let movieTitle = document.createElement('span')
+    let delButton = document.createElement('button')
+    
+    //add movie title
+    movieTitle.textContent = inputField.value
+    movieTitle.addEventListener('click', crossOffMovie)
+    movie.appendChild(movieTitle)
+    ul.appendChild(movie)
+
+    //adding x button
+    delButton.textContent = 'X'
+    delButton.addEventListener('click', deleteMovie)
+    movie.appendChild(delButton)
+    inputField.value = ''
+}
+
+form.addEventListener('submit', addMovie)
+
+function deleteMovie(event){
+    event.target.parentNode.remove()
+    message.textContent = event.target.parentNode.textContent + ' deleted!'
+    revealMessage()
+}
+
+function crossOffMovie(event){
+    event.target.classList.toggle('checked')
+    if (event.target.classList.contains('checked')){
+        message.textContent = event.target.textContent + ' watched!'
+    } else {
+        message.textContent = event.target.textContent + ' added back!'
+    }
+    revealMessage()
+}
+
+function revealMessage(){
+    message.classList.remove('hide')
+    setTimeout(() => {
+        message.classList.add('hide')
+    }, 1000)
+}
+
